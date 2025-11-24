e716ecdfcf01832884b6f50e6d5754afa2984023
Nguyen Long Nhat <nlnhttl21@gmail.com>
2025-11-24 23:24:42 +0700

    khoi dong gamingHouse

---
+++ src/main/java/com/lugx/gamingHouse/GamingHouseApplication.java
@@ -0,0 +1,13 @@
+package com.lugx.gamingHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lugx/gamingHouse/GamingHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lugx.gamingHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
