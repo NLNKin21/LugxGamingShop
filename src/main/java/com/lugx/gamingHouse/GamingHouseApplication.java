c087042639b4c3c238cf0d45329ff25809fcbf62
Nguyen Long Nhat <nlnhttl21@gmail.com>
2025-11-25 00:28:43 +0700

    enable jdbc & sercurity

--- src/main/java/com/lugx/gamingHouse/GamingHouseApplication.java
+++ src/main/java/com/lugx/gamingHouse/GamingHouseApplication.java
@@ -8,6 +8,7 @@ public class GamingHouseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamingHouseApplication.class, args);
+		
 	}
 
 }
---
+++ src/main/java/com/lugx/gamingHouse/HelloController.java
@@ -0,0 +1,14 @@
+package com.lugx.gamingHouse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World from Spring Boot!";
+    }
+
+}
